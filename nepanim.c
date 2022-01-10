f08a44f48c89e4e7b6cf72a8711dbc4c8e527d0c
NEPETAISCUTE <nepecute@penguin.lxd>
2022-01-10 21:32:33 +0100

    nice animation :p

--- nepanim.c
+++ nepanim.c
@@ -7,12 +7,12 @@ OBJ_AFFINE* obj_aff_buffer = (OBJ_AFFINE*)obj_buffer;
 int main()
 {
     memcpy32(&tile_mem[4][0], sprites,  sprites_size);
-    memcpy32(pal_obj_mem, sprPal, sprPal_size);
+    memcpy32(pal_obj_mem, sprPal, sprPal_size / 4);
 
     oam_init(obj_buffer, 128);
     REG_DISPCNT = DCNT_MODE0 | DCNT_OBJ | DCNT_OBJ_1D;
 
-
+    int frame = 0;
     u32 tid = 0;
     OBJ_ATTR* anim = &obj_buffer[0];
 
@@ -26,12 +26,17 @@ int main()
     while(true)
     {
         vid_vsync();
-        if(tid>3)
-            tid = 0;
+        if(frame%5==0)
+        {
+            tid+=4;
+            if(tid>3*4)
+                tid = 0;
 
-        anim->attr2= ATTR2_BUILD(tid, 0, 0);
+            anim->attr2= ATTR2_BUILD(tid, 0, 0);
 
-        oam_copy(oam_mem, obj_buffer, 1);
+            oam_copy(oam_mem, obj_buffer, 1);
+        }
+        frame++;
     }
     return 0;
 }
